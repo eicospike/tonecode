21019537c4f7731b9a01affabe31697021bbef6a
eicospike <eicospike@sdf.org>
2014-03-01 11:46:35 +0000

    Arguments

--- demo.x
+++ demo.x
@@ -1,7 +1,6 @@
 echo "$1"|minimodem --tx --alsa --ascii -f Noise.wav 100
 
-
-echo `./wavean2.py` |tr -d ' '|cut -c 4- | sed 's|..........|&\n|g'|sed 's|.| |8'|sed 's|.| |9'|sed 's|.| |10'|rev|sed 's/^ *//'>wavedump.bin
+echo `./wavean2.py $(echo "$1"|wc -c)` |tr -d ' '|cut -c 4- | sed 's|..........|&\n|g'|sed 's|.| |8'|sed 's|.| |9'|sed 's|.| |10'|rev|sed 's/^ *//'>wavedump.bin
 
 ./prepare.x wavedump.bin
 
--- wavean2.py
+++ wavean2.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python2.7
-
+import sys
+import os
 import wave
 import numpy as np
 from numpy import fft
 from pylab import *
 
-
+statinfo = os.stat('Noise.wav')
+sw = statinfo.st_size
 
 wr = wave.open('Noise.wav', 'rb')
-sz = 21184100
+sz = sw*8
 da = np.fromstring(wr.readframes(sz), dtype=np.int16)
-left, right = da[0::2], da[1::2]
+left = da[0::2]
 l = len(left)
-r = len(right)
 x = np.arange(l)
-y = np.arange(r)
 g = len(x)
-h = len(y)
-
+z = sys.argv[1]
+zz = int(z)
+zz = zz * 10
+t = 0
 tt = 0
 tt2 = 240
-for t in range(0,600):
+for t in range(0,zz):
         EE = left[tt:tt2]
         tt = tt + 240
         tt2 = tt2 + 240
-        lf, rf = np.fft.rfft(EE), np.fft.rfft(right)
+        lf = np.fft.rfft(EE)
         if abs(lf[11]) > 2200000:
                 print 0
         if abs(lf[13]) > 2000000:
                 print 1
-

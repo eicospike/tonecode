c5bacba2e75dc5b50463d45e5702e7a516a25d75
eicospike <eicospike@sdf.org>
2014-03-02 04:22:22 +0000

    eico3000

--- tc3000/wavean.py
+++ tc3000/wavean.py
@@ -24,46 +24,156 @@ y = np.arange(r)
 g = len(x)
 h = len(y)
 
-
-
-zzz = 16
+zzz = 8
 
 tt = 0
 
 tt2 = zzz
-for t in range(0,7):
+for t in range(0,20):
         EE = left[tt:tt2]
         tt = tt + zzz
         tt2 = tt2 + zzz
         lf, rf = np.fft.rfft(da), np.fft.rfft(EE)
-        if abs(rf[2]) > 110000:
+        avg = abs(rf[1]) + abs(rf[3])
+        avg = avg / 2
+        if (abs(rf[2]) < avg):
             print 0
-        if abs(rf[1]) > 100000:
+        else:
             print 1
-        
+#        if (abs(rf[2]) > 60000):
+#            print 0
+#        if (abs(rf[1]) > 90000) and (abs(rf[2]) < 60000):
+#            print 1
+#        if (abs(rf[2]) > 60000) or (abs(rf[1]) < abs(rf[2])):
+#            print 0
+#        else:
+#            print 1
+#         if (abs(rf[1]) < abs(rf[2])) or 
+
+
 #print x,da
 #print g,h,lf
 
 plt.figure(1)
-a = plt.subplot(211)
-r = 2**16
-a.set_ylim([-32200, -32210])
-a.set_xlabel('time [s]')
-a.set_ylabel('sample value [-]')
-plt.plot(y, left)
+#a = plt.subplot(411)
+#r = 2**16
+#a.set_ylim([-32200, -32210])
+#a.set_xlabel('time [s]')
+#a.set_ylabel('sample value [-]')
+#plt.plot(y, left)
 
-b = plt.subplot(212)
-b.set_xscale('log')
-b.set_xlabel('frequency [Hz]')
-b.set_ylabel('|amplitude|')
+xstart = 5
+tt = 0
+tt2 = zzz
+start = zzz * xstart
+tt = tt + start
+tt2 = tt2 + start
+EE = left[tt:tt2]
+tt = tt + zzz
+tt2 = tt2 + zzz
+lf, rf = np.fft.rfft(da), np.fft.rfft(EE)
+
+a = plt.subplot(911)
+a.set_xscale('log')
+a.set_xlabel('frequency [Hz]')
 plt.plot(abs(rf))
 
-plt.figure(2)
-c = plt.subplot(211)
-c.set_ylim([30755, 33070])
-c.set_xlabel('time [s]')
-c.set_ylabel('sample value [-]')
-plt.plot(y, left)
+EE = left[tt:tt2]
+tt = tt + zzz
+tt2 = tt2 + zzz
+lf, rf = np.fft.rfft(da), np.fft.rfft(EE)
+
+b = plt.subplot(912)
+b.set_xscale('log')
+b.set_xlabel('frequency [Hz]')
+plt.plot(abs(rf))
+
+EE = left[tt:tt2]
+tt = tt + zzz
+tt2 = tt2 + zzz
+lf, rf = np.fft.rfft(da), np.fft.rfft(EE)
+
+c = plt.subplot(913)
+c.set_xscale('log')
+c.set_xlabel('frequency [Hz]')
+plt.plot(abs(rf))
+
+
+EE = left[tt:tt2]
+tt = tt + zzz
+tt2 = tt2 + zzz
+lf, rf = np.fft.rfft(da), np.fft.rfft(EE)
+
+c = plt.subplot(914)
+c.set_xscale('log')
+c.set_xlabel('frequency [Hz]')
+plt.plot(abs(rf))
+
+EE = left[tt:tt2]
+tt = tt + zzz
+tt2 = tt2 + zzz
+lf, rf = np.fft.rfft(da), np.fft.rfft(EE)
+
+c = plt.subplot(915)
+c.set_xscale('log')
+c.set_xlabel('frequency [Hz]')
+plt.plot(abs(rf))
+
+EE = left[tt:tt2]
+tt = tt + zzz
+tt2 = tt2 + zzz
+lf, rf = np.fft.rfft(da), np.fft.rfft(EE)
+
+c = plt.subplot(916)
+c.set_xscale('log')
+c.set_xlabel('frequency [Hz]')
+plt.plot(abs(rf))
+
+EE = left[tt:tt2]
+tt = tt + zzz
+tt2 = tt2 + zzz
+lf, rf = np.fft.rfft(da), np.fft.rfft(EE)
+
+c = plt.subplot(917)
+c.set_xscale('log')
+c.set_xlabel('frequency [Hz]')
+plt.plot(abs(rf))
+
+EE = left[tt:tt2]
+tt = tt + zzz
+tt2 = tt2 + zzz
+lf, rf = np.fft.rfft(da), np.fft.rfft(EE)
+
+c = plt.subplot(918)
+c.set_xscale('log')
+c.set_xlabel('frequency [Hz]')
+plt.plot(abs(rf))
+
+EE = left[tt:tt2]
+tt = tt + zzz
+tt2 = tt2 + zzz
+lf, rf = np.fft.rfft(da), np.fft.rfft(EE)
+
+c = plt.subplot(919)
+c.set_xscale('log')
+c.set_xlabel('frequency [Hz]')
+plt.plot(abs(rf))
+
+
+
+
+
+
+
+
+
+
+#plt.figure(2)
+#c = plt.subplot(313)
+#ccf.set_ylim([30755, 33070])
+#cfset_xlabel('time [s]')
+#ccf.set_ylabel('sample value [-]')
+#plt.plot(y, left)
 
 
     
--- testing/demo.x
+++ testing/demo.x
@@ -1,6 +1,6 @@
 echo "$1"|minimodem --tx --alsa --ascii -f Noise.wav 3000
 
-echo `./wavean2.py $(echo "$1"|wc -c)` |tr -d ' '|cut -c 4- | sed 's|..........|&\n|g'|sed 's|.| |8'|sed 's|.| |9'|sed 's|.| |10'|rev|sed 's/^ *//'>wavedump.bin
+echo `./wavean2.py $(echo "$1"|wc -c)` |tr -d ' '|cut -c 3- | sed 's|..........|&\n|g'|sed 's|.| |8'|sed 's|.| |9'|sed 's|.| |10'|rev|sed 's/^ *//'>wavedump.bin
 
 #echo `./wavean2.py $(echo "$1"|wc -c)` |tr -d ' '|sed 's|..........|&\n|g'|sed 's|.| |8'|sed 's|.| |9'|sed 's|.| |10'|rev|sed 's/^ *//'>wavedump.bin
 
